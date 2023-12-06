31fa870170b946b0b7e6c85ee133da62e0ab708b
kmi2170 (Kemmei Hamada) <sageglorieux2170@hotmail.com>
2023-12-05 22:00:35 -0800

    Update: remove Copyright and leave &copy;
    	modified:   src/components/Footer/index.tsx

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,16 +1,16 @@
-import { memo } from 'react';
-import Typography from '@material-ui/core/Typography';
+import { memo } from "react";
+import Typography from "@material-ui/core/Typography";
 
 const Footer = () => {
   const year = new Date().getFullYear();
 
   return (
-    <footer style={{ margin: '5px' }}>
+    <footer style={{ margin: "5px" }}>
       <Typography variant="body2">
-        Copyrihgt &copy; kmi {year}. All rights reserved. | Powered by&nbsp;
+        &copy; kmi {year}. All rights reserved. | Powered by&nbsp;
         <a
           href="https://www.openweathermap.org/"
-          rel="noopen noreferrer"
+          rel="noopener noreferrer"
           target="_blank"
         >
           OpenWeather.com
