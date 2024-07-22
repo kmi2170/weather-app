f53c945078835a41fac008228da9884631053a0b
kmi2170 (Kemmei Hamada) <sageglorieux2170@hotmail.com>
2024-07-22 01:15:19 -0700

    Fix cookie validation

--- src/utils/cookiesValidator.ts
+++ src/utils/cookiesValidator.ts
@@ -1,23 +1,25 @@
 export const isUnitsValid = (units: unknown) => {
   if (
     units &&
-    typeof units === 'string' &&
-    (units === 'imperial' || units === 'metric')
+    typeof units === "string" &&
+    (units === "imperial" || units === "metric")
   ) {
     return true;
   }
   return false;
 };
 
-const keysToCheck = ['city', 'region', 'country', 'lat', 'lon'];
+const keysToCheck = ["city", "region", "country", "lat", "lon"];
 
 export const isLocationValid = (location: unknown) => {
-  if (location && typeof location == 'object') {
+  if (location && typeof location == "object") {
     const keys = Object.keys(location);
-    if (JSON.stringify(keys) === JSON.stringify(keysToCheck)) {
-      return true;
+    for (const key of keys) {
+      if (keysToCheck.includes(key)) {
+        if (location[key] != null) return true;
+      }
+      return false;
     }
-    return false;
   }
   return false;
 };
