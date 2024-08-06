fea9a2afe033924453a4d9530c12f93d1c78b77c
kmi2170 (Kemmei Hamada) <sageglorieux2170@hotmail.com>
2024-08-06 01:53:47 -0700

    use sx for styling instead of makeStyles

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,71 +3,13 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
-import { Theme } from "@mui/material/styles";
-import makeStyles from "@mui/styles/makeStyles";
-import { grey, purple, red } from "@mui/material/colors";
+import { grey, red } from "@mui/material/colors";
 
 import { useAppSelector } from "../../store/hooks";
 import { useGetWeatherQuery } from "../../services/weatherApi";
 import { Weather } from "../../api/types";
 import Buttons from "./Buttons";
 import Link from "next/link";
-
-const useStyles = makeStyles((theme: Theme) => ({
-  appBar: {
-    background: "rgba(233, 213, 255, 0.8)",
-    borderRadius: 0,
-  },
-  toolBar: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "center",
-  },
-  text: {
-    textTransform: "capitalize",
-    color: theme.palette.primary.dark,
-    "&:hover": {
-      color: purple[500],
-    },
-    [theme.breakpoints?.down("sm")]: {
-      fontSize: "1.0rem",
-    },
-  },
-  textAlerts: {
-    textTransform: "capitalize",
-    color: red[500],
-    fontSize: "1.0rem",
-    "&:hover": {
-      color: purple[500],
-    },
-  },
-  textAlertsDisabled: {
-    textTransform: "capitalize",
-    color: grey[500],
-    fontSize: "1.0rem",
-  },
-  list: {
-    display: "flex",
-    flexDirection: "row",
-  },
-  listItem: {
-    padding: "0 1rem",
-    [theme.breakpoints?.down("md")]: {
-      padding: "0 0.5rem",
-    },
-    borderBottom: `3px solid transparent`,
-    "&:hover": {
-      borderBottom: `3px solid ${purple[500]}`,
-    },
-  },
-  listItemDisabled: {
-    padding: "0 1rem",
-    [theme.breakpoints?.down("md")]: {
-      padding: "0 0.5rem",
-    },
-    borderBottom: `3px solid transparent`,
-  },
-}));
 
 const LinksList = [
   { id: 0, name: "current" },
@@ -78,8 +20,6 @@ const LinksList = [
 ];
 
 const Navbar = () => {
-  const classes = useStyles();
-
   const units = useAppSelector((state) => state.weather.units);
   const lang = useAppSelector((state) => state.weather.lang);
   const location = useAppSelector((state) => state.weather.location);
@@ -93,21 +33,59 @@ const Navbar = () => {
   const isAlerts = !!(data as Weather)?.alerts;
 
   return (
-    <AppBar position="sticky" className={classes.appBar}>
-      <Toolbar variant="dense" className={classes.toolBar}>
-        <List dense disablePadding className={classes.list}>
+    <AppBar
+      position="sticky"
+      sx={{
+        background: "rgba(233, 213, 255, 0.8)",
+        borderRadius: 0,
+      }}
+    >
+      <Toolbar
+        variant="dense"
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+        }}
+      >
+        <List
+          dense
+          disablePadding
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+          }}
+        >
           {LinksList.map(({ id, name }) => (
             <ListItem
               key={id}
               dense
               disableGutters
-              className={classes.listItem}
+              sx={(theme) => ({
+                padding: "0 1rem",
+                [theme.breakpoints?.down("md")]: {
+                  padding: "0 0.5rem",
+                },
+                borderBottom: `3px solid transparent`,
+                "&:hover": {
+                  borderBottom: `3px solid ${theme.palette.primary.main}`,
+                },
+              })}
             >
               <Link href={`#${name}`}>
                 <Typography
                   variant="h6"
                   align="center"
-                  className={classes.text}
+                  sx={(theme) => ({
+                    textTransform: "capitalize",
+                    color: theme.palette.primary.dark,
+                    "&:hover": {
+                      color: theme.palette.primary.main,
+                    },
+                    [theme.breakpoints?.down("sm")]: {
+                      fontSize: "1.0rem",
+                    },
+                  })}
                 >
                   {name}
                 </Typography>
@@ -120,15 +98,28 @@ const Navbar = () => {
             dense
             disableGutters
             alignItems="center"
-            className={isAlerts ? classes.listItem : classes.listItemDisabled}
+            sx={(theme) => ({
+              padding: "0 1rem",
+              [theme.breakpoints?.down("md")]: {
+                padding: "0 0.5rem",
+              },
+              borderBottom: `3px solid transparent`,
+            })}
           >
             <Link href={`#${LinksList[4]}`}>
               <Typography
                 variant="h6"
                 align="center"
-                className={
-                  isAlerts ? classes.textAlerts : classes.textAlertsDisabled
-                }
+                sx={(theme) => ({
+                  textTransform: "capitalize",
+                  color: isAlerts ? red[500] : grey[500],
+                  fontSize: "1.0rem",
+                  "&:hover": isAlerts
+                    ? {
+                        color: theme.palette.primary.main,
+                      }
+                    : undefined,
+                })}
               >
                 Alerts
               </Typography>
