084a5ac925d55005c0fe79d4a9295aa4daefc838
kmi2170 (Kemmei Hamada) <sageglorieux2170@hotmail.com>
2024-09-03 00:10:31 -0700

    new file, organaize files

--- src/components/SearchLocationBar/index.tsx
+++ src/components/SearchLocationBar/index.tsx
@@ -1,49 +1,12 @@
 import { memo, useState } from "react";
-import Image from "next/image";
 
-import IconButton from "@mui/material/IconButton";
-import TextField from "@mui/material/TextField";
-import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
 
-import { useAppSelector, useAppDispatch } from "../../store/hooks";
-import { asyncThunkSearchLocation } from "../../slice/weatherAsyncThunk";
-import { setIsNotFound } from "../../slice/weatherSlice";
-import SearchLocationModal from "../Modals/SearchLocation/searchLocationModal";
+import SearchLocationModalContent from "../Modals/SearchLocation/searchLocationModalContent";
 import { MGlassIcon } from "../../assets/icons";
 
-const icon_search = "/icon-search.png";
-const icon_cancel = "/icon-cancel.png";
-
-const queryExamples = ["London", "Seattle,WA,USA", "Tokyo,Japan"];
-
 const SearchLocationBar = () => {
-  const isNotFound = useAppSelector((state) => state.weather.isNotFound);
-  const dispatch = useAppDispatch();
-
-  const [searchLocation, setSearchLocation] = useState<string>("");
-
-  const handleInput = (
-    e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
-  ) => {
-    setSearchLocation(e.target.value);
-  };
-
-  const handleClear: React.MouseEventHandler<HTMLButtonElement> = () => {
-    setSearchLocation("");
-    dispatch(setIsNotFound(false));
-  };
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (searchLocation) {
-      dispatch(asyncThunkSearchLocation(searchLocation)).catch((error) =>
-        console.error(error)
-      );
-    }
-  };
-
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
@@ -56,7 +19,7 @@ const SearchLocationBar = () => {
         aria-describedby="modal-modal-description"
         // onClose={handleClose}
       >
-        <SearchLocationModal handleClose={handleClose} />
+        <SearchLocationModalContent handleClose={handleClose} />
       </Modal>
 
       <div
@@ -100,88 +63,6 @@ const SearchLocationBar = () => {
           </Box>
         </div>
       </div>
-
-      <form onSubmit={handleSubmit}>
-        <Box
-          sx={(theme) => ({
-            backgroundColor: "#fff",
-            borderRadius: 30,
-            margin: "0 5vw",
-            [theme.breakpoints?.up("sm")]: {
-              margin: "0 10vw",
-            },
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-          })}
-        >
-          {/* <IconButton type="submit" size="large">
-            <Image
-              src={icon_search}
-              alt="search button"
-              width={20}
-              height={20}
-            />
-          </IconButton> */}
-          <Box
-            sx={{
-              flexGrow: 1,
-            }}
-          >
-            {/* <TextField
-              fullWidth
-              type="text"
-              variant="standard"
-              value={searchLocation}
-              onChange={handleInput}
-              placeholder="Search Location; City,State,Country"
-              style={{ borderColor: "white" }}
-              onClick={handleOpen}
-            /> */}
-          </Box>
-          {/* <IconButton onClick={handleClear} size="large">
-            <Image
-              src={icon_cancel}
-              alt="clear button"
-              width={20}
-              height={20}
-            />
-          </IconButton> */}
-        </Box>
-      </form>
-      {isNotFound && (
-        <Box
-          sx={(theme) => ({
-            color: theme.palette.primary.dark,
-            marginTop: ".5rem",
-          })}
-        >
-          <Typography variant="h5" align="center">
-            No Place Found
-          </Typography>
-          <Typography
-            variant="h6"
-            align="center"
-            style={{ fontStyle: "italic" }}
-          >
-            Examples (case insensitive)
-          </Typography>
-          <Box
-            sx={{
-              fontStyle: "italic",
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "space-around",
-            }}
-          >
-            {queryExamples.map((example) => (
-              <Typography key={example} variant="h6" align="center">
-                {example}
-              </Typography>
-            ))}
-          </Box>
-        </Box>
-      )}
     </div>
   );
 };
