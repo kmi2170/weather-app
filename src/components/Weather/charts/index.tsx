f4a99a1482d522bc0938d616dd87c51fd38893d7
kmi2170 (Kemmei Hamada) <sageglorieux2170@hotmail.com>
2024-09-05 20:51:56 -0700

    remove charttemp with weathter

--- src/components/Weather/charts/index.tsx
+++ src/components/Weather/charts/index.tsx
@@ -4,4 +4,3 @@ export { default as ChartPrecipitation } from "./ChartPrecipitation";
 export { default as ChartTemps } from "./ChartTemps";
 export { default as ChartWind } from "./ChartWind";
 export { default as ChartMinutely } from "./ChartMinutely";
-export { default as ChartTempWithWeather } from "./ChartTempWithWeather";
