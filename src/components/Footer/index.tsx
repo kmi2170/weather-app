8a94d639e14364c397d07565823eb1f3ce236cb3
kmi2170 (Kemmei Hamada) <sageglorieux2170@hotmail.com>
2024-10-21 01:00:13 -0700

    typography as footer component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,19 +5,23 @@ const Footer = () => {
   const year = new Date().getFullYear();
 
   return (
-    <footer style={{ margin: "20px 10px 30px 0" }}>
-      <Typography variant="body2">
-        &copy; {year} Kemmei H. | Powered by OpenWeather.com
-        {/* <a
+    <Typography
+      component="footer"
+      variant="body2"
+      sx={{
+        margin: "20px 10px 30px 0",
+      }}
+    >
+      &copy; {year} Kemmei H. | Powered by OpenWeather.com
+      {/* <a
           href="https://www.openweathermap.org/"
           rel="noopener noreferrer"
           target="_blank"
         >
           OpenWeather.com
         </a> */}
-        .
-      </Typography>
-    </footer>
+      .
+    </Typography>
   );
 };
 
