59225f8fb546e0d0bd8192a797ed02d0ffc723b2
kmi2170 (Kemmei Hamada) <sageglorieux2170@hotmail.com>
2024-10-30 22:14:13 -0700

    token test

--- src/app/api/weathermap/route.ts
+++ src/app/api/weathermap/route.ts
@@ -2,8 +2,13 @@ import { NextResponse } from "next/server";
 import { fetchWeatherMap } from "../../../api/lib/fetchWeatherMap";
 import { WeatherMapLayerKeys, WeatherMapQuery } from "../../../api/types/map";
 
+import { cookies } from "next/headers";
+
 export async function GET(req: Request) {
   try {
+    const cookieStore = await cookies();
+    const token = cookieStore.get("__vercel_live_token");
+
     const url = new URL(req?.url as string);
     const searchParams = url.searchParams;
 
@@ -31,6 +36,9 @@ export async function GET(req: Request) {
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "GET",
         "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        // "Set-Cookie":
+        //   "cookieName=cookieValue; SameSite=None; Secure; HttpOnly; Path=/",
+        "Set-Cookie": `token=${token?.value}`,
       },
     });
   } catch (error) {
