d791b7e7e2155e6783b221dfc992e1f1bc2e9514
kmi2170 (Kemmei Hamada) <sageglorieux2170@hotmail.com>
2024-11-02 21:30:14 -0700

    basic setup drawer for mobile

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,10 +1,12 @@
-import React, { memo, useEffect } from "react";
+import React, { memo, useEffect, useState } from "react";
 
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
 import { grey } from "@mui/material/colors";
 
 import { useAppSelector } from "../../store/hooks";
@@ -13,6 +15,7 @@ import { Weather } from "../../api/types/weather";
 import Buttons from "./Buttons";
 import Link from "next/link";
 import { Box } from "@mui/material";
+import { opendir } from "fs";
 
 const LinksList = [
   { id: "top", name: "current" },
@@ -21,6 +24,7 @@ const LinksList = [
   { id: "daily", name: "daily" },
   { id: "charts", name: "charts" },
   { id: "map", name: "map" },
+  { id: "alerts", name: "alerts" },
 ];
 
 const Navbar = () => {
@@ -33,7 +37,7 @@ const Navbar = () => {
     lang,
   });
 
-  const handleScroll = (e: React.MouseEvent, id: string) => {
+  const handleScrollToId = (e: React.MouseEvent, id: string) => {
     e.preventDefault();
     const element = document.querySelector(`#${id}`);
     if (element) {
@@ -50,106 +54,53 @@ const Navbar = () => {
 
   const isAlerts = !!(data as Weather)?.alerts;
 
+  const [openDrawer, setOpenDrawer] = useState(false);
+
+  const toggle = (newOpenDrawer: boolean) => {
+    setOpenDrawer(newOpenDrawer);
+  };
+  console.log({ openDrawer });
+
   return (
     <AppBar
       position="sticky"
-      sx={{ background: "rgba(233, 213, 255, 0.9)", borderRadius: 0 }}
+      sx={{
+        background: "rgba(233, 213, 255, 0.9)",
+        borderRadius: 0,
+        height: "3.5rem",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
     >
-      <Toolbar
-        variant="dense"
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-around",
-        }}
-      >
-        <List
-          dense
-          disablePadding
-          sx={(theme) => ({
-            position: "relative",
-            display: "flex",
-            maxWidth: "80%",
-            paddingRight: "2rem",
-            paddingLeft: "2rem",
-            flexDirection: "row",
-            overflowX: "auto",
-            msOverflowStyle: "none", // IE and Edge
-            scrollbarWidth: "none", // Firefox
-            "&::-webkit-scrollbar": {
-              display: "none", // Chrome, Safari, and Opera
-            },
-          })}
-        >
-          {LinksList.map(({ id, name }) => (
-            <ListItem
-              key={id}
-              dense
-              disableGutters
-              sx={(theme) => ({
-                padding: "0 1rem",
-                [theme.breakpoints?.down("md")]: { padding: "0 0.5rem" },
-              })}
-            >
-              <Link href={`#${id}`} passHref>
-                <Typography
-                  variant="h6"
-                  align="center"
-                  sx={(theme) => ({
-                    textTransform: "capitalize",
-                    color: theme.palette.primary.dark,
-                    "&:active": { color: theme.palette.primary.main },
-                    [theme.breakpoints?.down("sm")]: { fontSize: "1.0rem" },
-                  })}
-                  onClick={(e) => handleScroll(e, `${id}`)}
-                >
-                  {name}
-                </Typography>
-              </Link>
-            </ListItem>
-          ))}
-
-          <ListItem
-            key="alert"
-            dense
-            disableGutters
-            alignItems="center"
+      <Toolbar variant="dense" sx={{}}>
+        <Box sx={{ display: { xs: "block", md: "none" } }}>
+          <Button onClick={() => toggle(true)}>Open</Button>
+          <Drawer
+            open={openDrawer}
+            onClose={() => toggle(false)}
             sx={(theme) => ({
-              padding: "0 1rem",
-              [theme.breakpoints?.down("md")]: { padding: "0 0.5rem" },
+              "& .MuiDrawer-paper": {
+                borderRadius: 0,
+                backgroundColor: theme.palette.primary.light,
+              },
             })}
           >
-            <Link href={`#alerts`} passHref>
-              <Typography
-                variant="h6"
-                align="center"
-                sx={(theme) => ({
-                  textTransform: "capitalize",
-                  color: isAlerts ? theme.palette.warning.main : grey[500],
-                  fontSize: "1.0rem",
-                  "&:active": isAlerts
-                    ? { color: theme.palette.warning.light }
-                    : undefined,
-                })}
-                onClick={(e) => {
-                  console.log({ isAlerts });
-                  if (isAlerts) {
-                    handleScroll(e, `alerts`);
-                  } else {
-                    e.preventDefault();
-                  }
-                }}
-              >
-                Alerts
-              </Typography>
-            </Link>
-          </ListItem>
-        </List>
+            <Box
+              sx={(theme) => ({
+                width: 250,
+              })}
+              // onClick={() => toggle(false)}
+            >
+              <NavContent isAlerts={isAlerts} />
+            </Box>
+          </Drawer>
+        </Box>
 
-        <MenuRightBlur />
-        <MenuLeftBlur />
-
-        <Buttons />
+        <Box sx={{ display: { xs: "none", md: "block" } }}>
+          <NavContent isAlerts={isAlerts} />
+        </Box>
       </Toolbar>
     </AppBar>
   );
@@ -157,34 +108,156 @@ const Navbar = () => {
 
 export default memo(Navbar);
 
-const MenuRightBlur = memo(() => {
-  return (
-    <Box
-      sx={{
-        position: "absolute",
-        top: 0,
-        left: 0,
-        width: "15%",
-        height: "100%",
-        backgroundImage:
-          "linear-gradient(90deg,rgba(233, 213, 255, 0.9), rgba(233, 213, 255, 0.0))",
-      }}
-    />
-  );
-});
+type NavContentProps = { isAlerts: boolean };
 
-const MenuLeftBlur = memo(() => {
+const NavContent = (props: NavContentProps) => {
+  const { isAlerts } = props;
+
+  const handleScrollToId = (e: React.MouseEvent, id: string) => {
+    e.preventDefault();
+    const element = document.querySelector(`#${id}`);
+    if (element) {
+      window.scrollTo({
+        behavior: "smooth",
+        top:
+          element.getBoundingClientRect().top -
+          document.body.getBoundingClientRect().top -
+          40,
+      });
+      // element.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
+  };
+
   return (
     <Box
       sx={{
-        position: "absolute",
-        top: 0,
-        right: 0,
-        width: "30%",
-        height: "100%",
-        backgroundImage:
-          "linear-gradient(90deg,rgba(233, 213, 255, 0.5), rgba(233, 213, 255, 0.9))",
+        display: "flex",
+        flexDirection: {
+          xs: "column",
+          md: "row",
+        },
+        gap: {
+          xs: "2rem",
+          md: "1rem",
+        },
+        marginTop: {
+          xs: "3rem",
+          md: 0,
+        },
       }}
-    />
+    >
+      <List
+        dense
+        disablePadding
+        sx={(theme) => ({
+          display: "flex",
+          flexDirection: {
+            xs: "column",
+            md: "row",
+          },
+          gap: {
+            xs: "1.25rem",
+            md: 0,
+          },
+          paddingLeft: {
+            xs: "2rem",
+            md: 0,
+          },
+        })}
+      >
+        {LinksList.map(({ id, name }) => (
+          <ListItem
+            key={id}
+            dense
+            disableGutters
+            sx={(theme) => ({
+              padding: "0 1rem",
+              [theme.breakpoints?.down("md")]: { padding: "0 0.5rem" },
+            })}
+          >
+            <Link href={`#${id}`} passHref>
+              {id === "alerts" ? (
+                <Typography
+                  variant="h6"
+                  align="center"
+                  sx={(theme) => ({
+                    textTransform: "capitalize",
+                    color: isAlerts ? theme.palette.warning.main : grey[500],
+                    fontSize: "1.0rem",
+                    "&:active": isAlerts
+                      ? { color: theme.palette.warning.light }
+                      : undefined,
+                  })}
+                  onClick={(e) => {
+                    console.log({ isAlerts });
+                    if (isAlerts) {
+                      handleScrollToId(e, `alerts`);
+                    } else {
+                      e.preventDefault();
+                    }
+                  }}
+                >
+                  {name}
+                </Typography>
+              ) : (
+                <Typography
+                  variant="h6"
+                  align="center"
+                  sx={(theme) => ({
+                    textTransform: "capitalize",
+                    color: theme.palette.primary.dark,
+                    "&:active": { color: theme.palette.primary.main },
+                    [theme.breakpoints?.down("sm")]: {
+                      fontSize: "1.0rem",
+                    },
+                  })}
+                  onClick={(e) => handleScrollToId(e, `${id}`)}
+                >
+                  {name}
+                </Typography>
+              )}
+            </Link>
+          </ListItem>
+        ))}
+      </List>
+
+      <Buttons />
+    </Box>
   );
-});
+};
+
+// const MenuRightBlur = memo(() => {
+//   return (
+//     <Box
+//       sx={{
+//         position: "absolute",
+//         top: 0,
+//         left: 0,
+//         width: "1.5rem",
+//         // width: "15%",
+//         height: "100%",
+//         border: "1px solid blue",
+//         backgroundImage:
+//           "linear-gradient(90deg,rgba(233, 213, 255, 0.9), rgba(233, 213, 255, 0.0))",
+//       }}
+//     />
+//   );
+// });
+
+// const MenuLeftBlur = memo(() => {
+//   return (
+//     <Box
+//       sx={{
+//         position: "absolute",
+//         top: 0,
+//         right: 0,
+//         width: "1.5rem",
+//         // width: "30%",
+//         height: "100%",
+//         border: "1px solid green",
+//         backgroundImage:
+//           "linear-gradient(90deg,rgba(233, 213, 255, 0.5), rgba(233, 213, 255, 0.9))",
+//       }}
+//     />
+//   );
+// });
