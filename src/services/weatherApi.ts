327bd3b4d6759157334d90e316372c554e9d6dda
kmi2170 (Kemmei Hamada) <sageglorieux2170@hotmail.com>
2025-04-26 21:54:25 -0700

    remove string type from return value

--- src/services/weatherApi.ts
+++ src/services/weatherApi.ts
@@ -21,7 +21,7 @@ export const weatherApi = createApi({
   // keepUnusedDataFor: 30,
   // refetchOnMountOrArgChange: 30,
   endpoints: (builder) => ({
-    getWeather: builder.query<Weather | string, WeatherQuery>({
+    getWeather: builder.query<Weather, WeatherQuery>({
       query: ({ lat, lon, units, lang }) => {
         return `weather?lat=${lat}&lon=${lon}&units=${units}&lang=${lang}`;
       },
