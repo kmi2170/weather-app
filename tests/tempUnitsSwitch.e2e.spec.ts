f09d24d2bf28afa5f89f2d32b438efa34f2f8566
kmi2170 (Kemmei Hamada) <sageglorieux2170@hotmail.com>
2025-04-28 20:30:01 -0700

    add tests on temp units siwtch and search modal

---
+++ tests/tempUnitsSwitch.e2e.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("temperature units switch", () => {
+  test("Should display the temperature in Celsius by clicking the Celsius button", async ({
+    page,
+  }) => {
+    await page.goto("http://localhost:3000/");
+    await page.getByRole("button", { name: "°C" }).click();
+    await expect(page.locator("h3").getByText("°C")).toBeVisible();
+  });
+
+  test("Should display the temperature in Fahrenheit by clicking the Fahrenheit button", async ({
+    page,
+  }) => {
+    await page.goto("http://localhost:3000/");
+    await page.getByRole("button", { name: "°F" }).click();
+    await expect(page.locator("h3").getByText("°F")).toBeVisible();
+  });
+});
