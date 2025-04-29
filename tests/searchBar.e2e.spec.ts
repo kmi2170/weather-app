eb28025c56afeb0959602a493be48660e1c15b45
kmi2170 (Kemmei Hamada) <sageglorieux2170@hotmail.com>
2025-04-28 23:14:23 -0700

    refactor

--- tests/searchBar.e2e.spec.ts
+++ tests/searchBar.e2e.spec.ts
@@ -1,9 +1,12 @@
 import { test, expect } from "@playwright/test";
 
+test.beforeEach("open page", async ({ page }) => {
+  await page.goto("http://localhost:3000/");
+});
+
 test.describe("Search bar", () => {
   test.describe("Open search modal", () => {
     test("should open search modal by pressing control+k", async ({ page }) => {
-      await page.goto("http://localhost:3000/");
       await page.locator("body").press("ControlOrMeta+k");
       await expect(page.getByTestId("search-location-modal")).toBeVisible();
     });
@@ -11,7 +14,6 @@ test.describe("Search bar", () => {
     test("should open search modal by clicking on search bar", async ({
       page,
     }) => {
-      await page.goto("http://localhost:3000/");
       await page.getByRole("textbox", { name: "Search City" }).click();
       await expect(page.getByTestId("search-location-modal")).toBeVisible();
     });
@@ -21,7 +23,6 @@ test.describe("Search bar", () => {
     test("should close search modal by pressing escape key", async ({
       page,
     }) => {
-      await page.goto("http://localhost:3000/");
       await page.getByRole("textbox", { name: "Search City" }).click();
       await page.locator("body").press("Escape");
       await expect(page.getByTestId("search-location-modal")).toBeHidden();
@@ -30,7 +31,6 @@ test.describe("Search bar", () => {
     test("should close search modal by clicking on close button", async ({
       page,
     }) => {
-      await page.goto("http://localhost:3000/");
       await page.getByRole("textbox", { name: "Search City" }).click();
       await page.getByRole("img").first().click();
       await expect(page.getByTestId("search-location-modal")).toBeHidden();
@@ -41,7 +41,6 @@ test.describe("Search bar", () => {
     test("should display the city on the list on the search modal by type in city name", async ({
       page,
     }) => {
-      await page.goto("http://localhost:3000/");
       await page.getByRole("textbox", { name: "Search City" }).click();
       await page
         .getByRole("textbox", { name: "Enter City Name" })
@@ -54,7 +53,6 @@ test.describe("Search bar", () => {
     test("should display the weather information of the city by clicking the city name on the list on the search modal", async ({
       page,
     }) => {
-      await page.goto("http://localhost:3000/");
       await page.getByRole("textbox", { name: "Search City" }).click();
       await page
         .getByRole("textbox", { name: "Enter City Name" })
--- tests/tempUnitsSwitch.e2e.spec.ts
+++ tests/tempUnitsSwitch.e2e.spec.ts
@@ -1,10 +1,13 @@
 import { test, expect } from "@playwright/test";
 
+test.beforeEach("open page", async ({ page }) => {
+  await page.goto("http://localhost:3000/");
+});
+
 test.describe("temperature units switch", () => {
   test("Should display the temperature in Celsius by clicking the Celsius button", async ({
     page,
   }) => {
-    await page.goto("http://localhost:3000/");
     await page.getByRole("button", { name: "°C" }).click();
     await expect(page.locator("h3").getByText("°C")).toBeVisible();
   });
@@ -12,7 +15,6 @@ test.describe("temperature units switch", () => {
   test("Should display the temperature in Fahrenheit by clicking the Fahrenheit button", async ({
     page,
   }) => {
-    await page.goto("http://localhost:3000/");
     await page.getByRole("button", { name: "°F" }).click();
     await expect(page.locator("h3").getByText("°F")).toBeVisible();
   });
